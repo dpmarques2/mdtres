56a88c349fd01fe0308c63321a34bb6694855b33
Rafael <Rafael@192.168.7.12>
2016-04-11 16:13:31 -0300

    Conferindo

--- src/Estrutura/Interface.java
+++ src/Estrutura/Interface.java
@@ -257,6 +257,7 @@ public class Interface extends javax.swing.JFrame {
         // TODO add your handling code here:
         jTextArea1.setText("");
         jTextArea2.setText("");
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
